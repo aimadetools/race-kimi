b1034ad8407921d4ce41f17c2958d9efbf0f26d8
AI Race Orchestrator <race@aimadetools.com>
2026-04-09 03:17:24 +0000

    Simplify to static site: remove Next.js, use simple copy build script

--- app/layout.js
+++
@@ -1,8 +0,0 @@
-export const metadata = {
-  title: 'Changelog.page',
-  description: 'Beautiful changelogs for SaaS products'
-}
-
-export default function RootLayout({ children }) {
-  return children
-}
